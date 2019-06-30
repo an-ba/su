01914e85c1964e568f2f0d90db7a32765ed5878e
aba <aba@a.ba>
2019-06-30 17:48:05 +0200

    next button

---
+++ dev/main.py
@@ -0,0 +1,38 @@
+from starlette.applications import Starlette
+from starlette.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
+
+import yaml #PyYAML
+import random
+
+
+def read_phrases(location="./phrases.yml"):
+    with open(location, 'r') as stream:
+        try:
+            phrases = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+        return phrases
+
+def phrase_picker(key):
+    phrases = read_phrases()
+    phrase = random.choice(phrases[key])
+    return phrase
+
+def phrase():
+    nl = '\n'
+    final_string = f"Ich {phrase_picker('aktion1')} {phrase_picker('aktion2')} {phrase_picker('objekt')}, weil {phrase_picker('reason')}. {phrase_picker('nachsatz')}"
+    return final_string
+
+
+
+templates = Jinja2Templates(directory='templates')
+
+app = Starlette(debug=True)
+app.mount('/static', StaticFiles(directory='static'), name='static')
+
+
+
+@app.route('/')
+async def homepage(request):
+    return templates.TemplateResponse('index.html', {'request': request, "string":phrase()})
