04530f842fea6c47f07f27d9e332a0f5aba5c8aa
aba <aba@a.ba>
2019-06-30 15:09:33 +0200

    generator mit yaml

--- gen.py
+++ gen.py
@@ -2,14 +2,14 @@ import yaml #PyYAML
 import random
 
 
+with open("./phrasen.yml", 'r') as stream:
+    try:
+        phrasen = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
 
-aktion1 = ["debugge", "teste"]
-aktion2 = ["die Nacharbeiten", "den Code-Review", "die Nacharbeiten"]
-objekt = ["zur Zwischenschicht", "zum Formular-Layer", "für das Abbrechermailing"]
-reason = ["es in der Queue hochgerutscht ist", "es in der Queue runtergerutscht ist", "es jetzt hochpriorisiert wurde", "es jetzt runterpriorisiert wurde"]
-nachsatz = ["An der Stelle.", "Habe ich etwa Peakwork gesagt??", "Und dann schaue ich noch was in der Queue liegt"]
 
-output = f"Ich {random.choice(aktion1)} {random.choice(aktion2)}  {random.choice(objekt)},weil {random.choice(reason)}. {random.choice(nachsatz)}"
+output = f"Ich {random.choice(phrasen['aktion1'])} {random.choice(phrasen['aktion2'])} {random.choice(phrasen['objekt'])}, weil {random.choice(phrasen['reason'])}. {random.choice(phrasen['nachsatz'])}"
 
 
 print(output)
